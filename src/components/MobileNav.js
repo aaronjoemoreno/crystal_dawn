ffd970c2229fd1ced0673ba411315e6aa7829e05
Aaron Moreno <aaron.j.moreno@gmail.com>
2022-02-16 22:40:51 -0700

    fixed path to component

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -2,16 +2,12 @@ import React from 'react'
 import { slide as Menu } from 'react-burger-menu'
 
 const MobileNav = () => {
-  const showSettings = (event) => {
-    event.preventDefault();
-  }
 
   return (
     <Menu  width={ '100%' }>
       <a id="home" className="menu-item">Home</a>
       <a id="about" className="menu-item">About</a>
       <a id="contact" className="menu-item">Contact</a>
-      <a onClick={() => showSettings()} className="menu-item--small">Settings</a>
     </Menu>
   )
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import MobileNav from "./mobileNav"
+import MobileNav from "./MobileNav"
 import "./layout.css"
 
 const Layout = ({ children }) => {
