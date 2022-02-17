4d8039e7a38bf8e026156165dcec32d47ed138ac
Aaron Moreno <aaron.j.moreno@gmail.com>
2022-02-16 23:07:32 -0700

    trouble shooting component deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { About } from "../components/About"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <Header/>
-    <About/>
-  </Layout>
-)
+const IndexPage = () => {
+  return(
+    <Layout>
+      <Seo title="Home" />
+      <Header/>
+      <About/>
+    </Layout>
+  )
+}
 
 export default IndexPage
