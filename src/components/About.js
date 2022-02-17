0a9a3be2a1bc8f6cf3bce4db7d1485b06747705b
Aaron Moreno <aaron.j.moreno@gmail.com>
2022-02-16 22:30:38 -0700

    added intersection observer for animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
+import { useIntersection } from "react-use";
+import gsap from "gsap";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 50vh;
   background: var(--pink);
 
   .custom-shape-divider-top-1645069763 {
@@ -28,8 +30,58 @@ const Container = styled.div`
 
 `
 
+const AboutContainer = styled.div`
+  width: 100%;
+  height: auto;
+  background: var(--pink);
+
+  .section-second{
+    width: 80%;
+    margin: 0 auto;
+
+    p{
+      font-size: 3rem;
+    }
+  }
+`
+
 export const About = () => {
+
+  const sectionRef = useRef(null);
+  // All the ref to be observed
+  const intersection = useIntersection(sectionRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: .4
+  });
+
+  // Animation for fading in
+  const fadeIn = element => {
+    gsap.to(element, 1, {
+      opacity: 1,
+      y: -60,
+      ease: "power4.out",
+      stagger: {
+        amount: 0.3
+      }
+    });
+  };
+  // Animation for fading out
+  const fadeOut = element => {
+    gsap.to(element, 1, {
+      opacity: 0,
+      y: -20,
+      ease: "power4.out"
+    });
+  };
+
+  // checking to see when the vieport is visible to the user
+  intersection && intersection.intersectionRatio < .4
+    ? fadeOut(".fadeIn")
+    : fadeIn(".fadeIn");
+
   return (
+    <>
     <Container>
       <div class="custom-shape-divider-top-1645069763">
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -37,5 +89,16 @@ export const About = () => {
           </svg>
       </div>
     </Container>
+    <AboutContainer>
+      <div className="section-second" ref={sectionRef}>
+        <h1 className='fadeIn'>HELLO</h1>
+        <p className="fadeIn">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur reprehenderit quod distinctio magnam reiciendis sed excepturi doloremque! Sit vitae non similique voluptate omnis atque fugit quidem molestias! Iste, corporis adipisci.
+        <br/>
+        <br/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur reprehenderit quod distinctio magnam reiciendis sed excepturi doloremque! Sit vitae non similique voluptate omnis atque fugit quidem molestias! Iste, corporis adipisci.
+        </p>
+      </div>
+    </AboutContainer>
+    </>
   )
 }
